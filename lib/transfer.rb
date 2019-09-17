261b3c26081109b7a85376c54545f487647b286d
Panela Leung <leungpanela@gmail.com>
2019-09-17 14:28:05 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,7 +20,7 @@ class Transfer
   end
 
   def execute_transaction
-    if sender.status == "closed"
+    if sender.@status == "closed"
       binding.pry
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
